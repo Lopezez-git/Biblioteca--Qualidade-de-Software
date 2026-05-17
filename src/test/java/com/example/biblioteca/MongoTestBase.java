b5673ef6f0af036cc7838fab017da3a7bbbd0e23
Allan Lopes <lopes.shigame@gmail.com>
2026-05-17 17:21:31 -0300

    Alterando MongoTestBase

--- src/test/java/com/example/biblioteca/MongoTestBase.java
+++ src/test/java/com/example/biblioteca/MongoTestBase.java
@@ -4,19 +4,20 @@ import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 public abstract class MongoTestBase {
 
     @Container
-    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7")
-            .withReuse(true);
-
-    static {
-        mongoDBContainer.start();
-    }
+    static MongoDBContainer mongoDBContainer =
+            new MongoDBContainer("mongo:7");
 
     @DynamicPropertySource
     static void mongoProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+        registry.add(
+                "spring.data.mongodb.uri",
+                mongoDBContainer::getReplicaSetUrl
+        );
     }
 }
