e0e806a903e01ef0fe2d6d9877b422ca41345695
Allan Lopes <lopes.shigame@gmail.com>
2026-05-17 17:09:50 -0300

    Fix o erro de conexão do mongo

--- src/test/java/com/example/biblioteca/MongoTestBase.java
+++ src/test/java/com/example/biblioteca/MongoTestBase.java
@@ -4,14 +4,12 @@ import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
 public abstract class MongoTestBase {
 
     @Container
-    static final MongoDBContainer mongoDBContainer =
-            new MongoDBContainer("mongo:7");
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7")
+            .withReuse(true);
 
     static {
         mongoDBContainer.start();
@@ -19,9 +17,6 @@ public abstract class MongoTestBase {
 
     @DynamicPropertySource
     static void mongoProperties(DynamicPropertyRegistry registry) {
-        registry.add(
-                "spring.data.mongodb.uri",
-                mongoDBContainer::getReplicaSetUrl
-        );
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
     }
 }
